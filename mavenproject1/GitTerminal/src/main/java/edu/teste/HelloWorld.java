ef14dcfd70e792ec72ce4a4716137733185aaca6
wellingtonlins <wellingtonlins2013@gmail.com>
2016-07-08 22:07:41 -0300

    hello

---
+++ mavenproject1/GitTerminal/src/main/java/edu/teste/HelloWorld.java
@@ -0,0 +1,13 @@
+package edu.teste;
+
+/**
+ * Document   NovoClass
+ * @Date  08/07/2016 @Time 22:06:58
+ * @author Wellington Lins Claudino Duarte   
+ * @mail wellingtonlins2013@gmail.com
+ */ 
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
